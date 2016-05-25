c283d3e88ba7901edd2a16ceea6887505022e4cd
jopk <biasejok@gmail.com>
2016-05-25 20:25:58 +0200

    3 zgodnie ze wskazówką

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AAlg
+
+Dzielimy się zadaniami, ale każdy samodzielnie robi wykresy. Stąd plik *main.jl*, w nim ma znajdować się lista zaimplementowanych zadań. Natomiast do wykresów każdy robi sobie osobny plik którego nie wrzuca na gita. Do tego pliku wystarczy załączyć dodać linię:
+	*include("main.jl")*
+i nie trzeba się martwić o importowanie plików, bo to zapewnia osoba dodająca rozwiązanie zadania.
--- randperm.jl
+++ randperm.jl
@@ -1,38 +1,19 @@
 #!/bin/julia
 
-function randperm(len::Int)
-	return randperm(1:len)
-end
-
-function randperm(r::Range{T})
-	A = collect(r)
-	return randperm(A)
-end
-
-function randperm(A::Array)
-	Ap = copy(A)
-	r = 1:length(Ap)
-	for i = r
-		x = rand(r)
-		y = rand(r)
-		Ap[x], Ap[y] = Ap[y], Ap[x]
+function position(i::Int, n::Int)
+	sum = 0
+	for j = 1:n
+		x = abs(i - j)
+		sum += x
 	end
-	return A
+	expected = sum / n
 end
 
-function experyment(times::Int)
-	res = 0
-	for t in 1:times
-		A = randperm(10)
-		sum = 0;
-		for ai in 1:10
-			i = A[ai]
-			x = abs(ai - i)
-			sum += x
-		end
-		res += sum
+function expected_value(n)
+	E = Float64[]
+	for i = 1:n
+		x = position(i, n)
+		push!(E, x)
 	end
-	res = div(res, times)
-	return res
+	return E, sum(E)
 end
-
