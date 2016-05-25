a424171ba05b29674aa26d73fec8f1379949d952
jopk <biasejok@gmail.com>
2016-05-25 12:45:00 +0200

    3 w całości

--- randperm.jl
+++ randperm.jl
@@ -1,17 +1,38 @@
 #!/bin/julia
 
 function randperm(len::Int)
-	A = collect(1:len)
-	return randperm!(A)
+	return randperm(1:len)
 end
 
-function randperm!(A::Array)
-	r = 1:length(A)
+function randperm(r::Range{T})
+	A = collect(r)
+	return randperm(A)
+end
+
+function randperm(A::Array)
+	Ap = copy(A)
+	r = 1:length(Ap)
 	for i = r
 		x = rand(r)
 		y = rand(r)
-		A[x], A[y] = A[y], A[x]
+		Ap[x], Ap[y] = Ap[y], Ap[x]
 	end
 	return A
+end
+
+function experyment(times::Int)
+	res = 0
+	for t in 1:times
+		A = randperm(10)
+		sum = 0;
+		for ai in 1:10
+			i = A[ai]
+			x = abs(ai - i)
+			sum += x
+		end
+		res += sum
+	end
+	res = div(res, times)
+	return res
 end
 
