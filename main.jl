2af850f735daa5514ba6a944f139585a59d80b74
jopk <biasejok@gmail.com>
2016-06-04 01:23:21 +0200

    includy

--- main.jl
+++ main.jl
@@ -6,9 +6,11 @@
 include("geometric.jl") 	#1
 include("poisson.jl") 		#2
 include("randperm.jl") 		#3
-include("loser.jl") 		#13
 include("zadanie5.jl")
+include("quicksort.jl") 	#6
+include("mergesort.jl") 	#7
 include("zadanie8.jl")
 include("zadanie9.jl")
 include("zadanie10.jl")
 include("zadanie11.jl")
+include("loser.jl") 		#13
